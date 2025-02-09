6cc99b11b012f96abbf15a4bc014d114d8db067a
tanggao <19157682377@163.com>
2025-02-09 23:02:34 +0800

    init repository

---
+++ src/main/java/com/tanggao/rocketmq_leaning/RocketmqLeaningApplication.java
@@ -0,0 +1,13 @@
+package com.tanggao.rocketmq_leaning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqLeaningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqLeaningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tanggao/rocketmq_leaning/RocketmqLeaningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanggao.rocketmq_leaning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqLeaningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
